99a5a28c8976ff4eacc4c078a2ab007b21e02974
Enigmadie <gagen666@mail.ru>
2020-01-10 02:29:15 +0300

    fix front

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ import path from 'path';
 import Express from 'express';
 import addRoutes from './routes';
 
-const pathway = path.join(__dirname, '/public');
+const rootPath = path.join(__dirname, '../dist');
+// const appPath = path.join(__dirname, '..');
 
 export default () => {
-  console.log(pathway)
+  console.log(rootPath)
   const app = new Express();
   app.set('view engine', 'pug');
-  app.use('/assets', Express.static(pathway));
+  app.use('/assets', Express.static(rootPath));
   addRoutes(app);
   return app;
 }
